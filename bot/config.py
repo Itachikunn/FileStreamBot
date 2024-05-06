0e85faf1342697d4234a22b429b515a053dbf281
Itachikunn <160922473+Itachikunn@users.noreply.github.com>
2024-05-06 16:08:35 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -11,7 +11,7 @@ class Telegram:
     SECRET_CODE_LENGTH = int(env.get("SECRET_CODE_LENGTH", 12))
 
 class Server:
-    BASE_URL = env.get("https://filestreamzz-e29bb1d94044.herokuapp.com/")
+    BASE_URL = env.get("https://filestreamzz-e29bb1d94044.herokuapp.com")
     BIND_ADDRESS = env.get("BIND_ADDRESS", "0.0.0.0")
     PORT = int(env.get("PORT", 8080))
 
